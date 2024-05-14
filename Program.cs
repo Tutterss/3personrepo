8cac4e47d28476235184981994fb1ed1490cfd4d
Matthew39099 <VISSMJ1@student.op.ac.nz>
2024-05-14 15:38:30 +1200

    Hellooooooo

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.Write("Hello World 2 Electric Boogaloo!");
+            Console.ReadLine();
         }
     }
 }
