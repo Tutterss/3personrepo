63bbadb37e8d5543c99209136cdb4e9e3f1f5f64
Joshua Smith (1000125321) <SMITJA21@student.op.ac.nz>
2024-05-14 15:47:33 +1200

    Miles to km

--- Program.cs
+++ Program.cs
@@ -9,6 +9,20 @@
             Console.WriteLine("Hello, my name is Josh");
             Console.Write("Hello World 2 Electric Boogaloo!");
             Console.ReadLine();
+
+
+
+            //Josh's code to convert Miles to Kilometres
+            const double MILES_FACTOR = 1.609344;
+            double miles, kilometres;
+            string temp;
+            Console.Write("Please enter the value to Miles you would like to convert to kilometres ");
+            temp = Console.ReadLine();
+            miles = Convert.ToDouble(temp);
+            kilometres = miles * MILES_FACTOR;
+            Console.WriteLine($"The value you entered as miles converted to kilometres is equal to {kilometres:F2}km ");
+            Console.ReadLine();
         }
+    }
     }
 }
